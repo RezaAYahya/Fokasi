7b7fe070b5b5f42e50b2d2ba88b910b355e026ba
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-19 09:29:24 +0700

    Create footer for template

---
+++ application/views/template/footer.php
@@ -0,0 +1,41 @@
+<!-- Footer -->
+    <footer class="footer-section">
+        <div class="container">
+            <div class="footer-text">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="ft-about">
+                            <div class="logo">
+                                <a href="<?= base_url() . 'welcome/index' ?>">
+                                    <img src="<?= base_url() . 'assets/img/logo.png' ?>" alt="">
+                                </a>
+                            </div>
+                            <p>Sistem Informasi yang selalu memberikan kemudahan,<br /> Selalu ada dan terpercaya.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 offset-lg-1">
+                        <div class="ft-nav">
+                            <h6>Navigasi</h6>
+                            <a class="ft-link" href="about.html">Tentang Kami</a><br>
+                            <a class="ft-link" href="<?= base_url() . 'welcome/blog' ?>">Informasi Kesehatan</a><br>
+                            <a class="ft-link" href="search-dokter.html">Cari Dokter</a><br>
+                            <a class="ft-link" href="index.html">Cari Obat</a><br>
+                            <a class="ft-link" href="contact.html">Kontak Kami</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 offset-lg-1">
+                        <div class="ft-any">
+                            <h6>Lainnya</h6>
+                            <a class="ft-link" href="#">Syarat & Ketentuan</a><br>
+                            <a class="ft-link" href="#">Privasi</a><br>
+                            <a class="ft-link" href="<?= base_url() . 'register/dokter' ?>">Gabung Tim Dokter</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
+    <!-- Actionbar Animation.js -->
+    <script src="js/index.js"></script>
+</body>
