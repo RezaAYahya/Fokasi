abc69a71dd5c350394205f3cd9628412424b0939
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-19 09:40:13 +0700

    Create view informasi kesehatan

---
+++ application/views/blog.php
@@ -0,0 +1,17 @@
+    <!-- Header Rata -->
+    <div class="header-rata">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header-rata-text">
+                        <h2>Informasi Kesehatan</h2>
+                        <div class="btn-navi">
+                            <a href="./index.html">Home</a>
+                            <span>Informasi Kesehatan</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Header Rata -->
