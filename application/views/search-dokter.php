5b0fb0c188b36f5aa64fa8dc786298f2c23e0b2a
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-19 10:20:55 +0700

    Create view about and search dokter

---
+++ application/views/search-dokter.php
@@ -0,0 +1,17 @@
+    <!-- Header Rata -->
+    <div class="header-rata">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header-rata-text">
+                        <h2>Daftar Dokter</h2>
+                        <div class="btn-navi">
+                            <a href="./index.html">Home</a>
+                            <span>Daftar Dokter</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Header Rata -->
