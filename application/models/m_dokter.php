4238b8670f2095ddd225707e1a0413bb1ba91026
izarwh <ijul.harahap@gmail.com>
2020-04-22 21:44:40 +0700

    edit index karena di db ganti nama row

--- application/models/m_dokter.php
+++ application/models/m_dokter.php
@@ -6,7 +6,7 @@ class m_dokter extends CI_Model
 
     public function getprofile($username)
     {
-        $query = $this->db->where('username', $username)->get('tb_dokter')->row_array();
+        $query = $this->db->where('usernamedok', $username)->get('tb_dokter')->row_array();
         if ($query) {
             return $query;
         } else {
@@ -16,30 +16,30 @@ class m_dokter extends CI_Model
 
     public function setprofile($data)
     {
-        $this->db->set('username', $data['username']);
+        $this->db->set('usernamedok', $data['usernamedok']);
         $this->db->set('name', $data['name']);
         $this->db->set('phone_number', $data['phone_number']);
         $this->db->set('email', $data['email']);
         $this->db->set('bidang', $data['bidang']);
-        $this->db->where('username', $data['prevuser']);
+        $this->db->where('usernamedok', $data['prevuser']);
         $this->db->update('tb_member');
     }
 
     public function increaserating($username)
     {
-        $rating = $this->db->select('rating')->where('username', $username)->get('tb_dokter')->result();
+        $rating = $this->db->select('rating')->where('usernamedok', $username)->get('tb_dokter')->result();
         $rating = $rating + 1;
     }
 
     public function decreaserating($username)
     {
-        $rating = $this->db->select('rating')->where('username', $username)->get('tb_dokter')->result();
+        $rating = $this->db->select('rating')->where('usernamedok', $username)->get('tb_dokter')->result();
         $rating = $rating - 1;
     }
 
     public function getkomendokter($username)
     {
-        $query = $this->db->where('username', $username)->get('tb_komen');
+        $query = $this->db->where('usernamedok', $username)->get('tb_komen');
         if ($query->num_rows() > 0) {
             return $query->result_array();
         } else {
@@ -49,11 +49,11 @@ class m_dokter extends CI_Model
 
     public function addkomendokter($data)
     {
-        $username = $data['username'];
+        $username = $data['usernamedok'];
         $komentar = $data['komentar'];
         $id = $data['post_id'];
 
-        $this->db->where('post_id', $id)->set('username', $username);
+        $this->db->where('post_id', $id)->set('usernamedok', $username);
         $this->db->set('komentar', $komentar);
         $this->db->insert('tb_komen');
     }
