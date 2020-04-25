bd608f0d602f8df2bae4765205627cb849181b4f
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-25 16:08:44 +0700

    Create view cari obat

---
+++ application/views/daftar-obat.php
@@ -0,0 +1,52 @@
+    <!-- Header Rata -->
+    <div class="header-rata">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header-rata-text">
+                        <h2>Daftar Obat</h2>
+                        <div class="btn-navi">
+                            <a href="<?= base_url() . 'welcome/index' ?>">Home</a>
+                            <a href="<?= base_url() . 'welcome/searchObat' ?>">Kategori Obat</a>
+                            <span>Nama Kategori</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Header Rata -->
+
+    <!-- Obat Content-->
+    <center>
+        <table class="table" style="width: 800px;">
+            <thead style="color: white; background-color: dodgerblue;">
+                <tr>
+                    <th scope="col">No.</th>
+                    <th scope="col">Nama Obat</th>
+                    <th scope="col">Jenis Obat</th>
+                    <th scope="col">Harga</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th scope="row">
+                    <td>Bisolvon Extra</td>
+                    <td>Batuk dan Flu</td>
+                    <td>Rp. 33.000</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th scope="row">
+                    <td>Panadol Flu & Batuk</td>
+                    <td>Batuk dan Flu</td>
+                    <td>Rp. 22.500</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th scope="row">
+                    <td>Neosep Forte</td>
+                    <td>Batuk dan Flu</td>
+                    <td>Rp. 5.000</td>
+                </tr>
+        </table>
+    </center>
+    <!-- End Obat Content-->
---
+++ application/views/search-obat.php
@@ -0,0 +1,106 @@
+    <!-- Header Rata -->
+    <div class="header-rata">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header-rata-text">
+                        <h2>Daftar Obat</h2>
+                        <div class="btn-navi">
+                            <a href="<?= base_url() . 'welcome/index' ?>">Home</a>
+                            <span>Daftar Obat</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Header Rata -->
+
+    <!-- Obat Content -->
+    <div class="obat-content">
+        <div class="container">
+            <div class="nav-cards row no-gutters">
+                <div class="nav-cards_item">
+                    <button class="nav-cards_btn-obat" style="width: 200px; height: 70px;">
+                        <a style="color: rgb(46, 46, 46);" href="<?= base_url() . 'welcome/daftar_obat' ?>">
+                            <div>
+                                <span style="padding-right: 10px;" >Batuk & Flu</span>
+                                <img style="width: 50px;" src="https://d2qjkwm11akmwu.cloudfront.net/categories/200609_4-3-2020_14-7-9.png"/>
+                            </div>
+                        </a>
+                    </button>
+                </div>
+                <div class="nav-cards_item">
+                    <button class="nav-cards_btn-obat" style="width: 200px; height: 70px;">
+                        <a style="color: rgb(46, 46, 46);" href="<?= base_url() . 'welcome/index' ?>">
+                            <div>
+                                <span style="padding-right: 10px;" >Demam</span>
+                                <img style="width: 50px;" src="https://d2qjkwm11akmwu.cloudfront.net/categories/200609_4-3-2020_14-7-9.png"/>
+                            </div>
+                        </a>
+                    </button>
+                </div>
+                <div class="nav-cards_item">
+                    <button class="nav-cards_btn-obat" style="width: 200px; height: 70px;">
+                        <a style="color: rgb(46, 46, 46);" href="<?= base_url() . 'welcome/index' ?>">
+                            <div>
+                                <span style="padding-right: 10px;" >Pencernaan</span>
+                                <img style="width: 50px;" src="https://d2qjkwm11akmwu.cloudfront.net/categories/200609_4-3-2020_14-7-9.png"/>
+                            </div>
+                        </a>
+                    </button>
+                </div>
+                <div class="nav-cards_item">
+                    <button class="nav-cards_btn-obat" style="width: 200px; height: 70px;">
+                        <a style="color: rgb(46, 46, 46);" href="<?= base_url() . 'welcome/index' ?>">
+                            <div>
+                                <span style="padding-right: 10px;" >Alergi</span>
+                                <img style="width: 50px;" src="https://d2qjkwm11akmwu.cloudfront.net/categories/200609_4-3-2020_14-7-9.png"/>
+                            </div>
+                        </a>
+                    </button>
+                </div>
+                <div class="nav-cards_item">
+                    <button class="nav-cards_btn-obat" style="width: 200px; height: 70px;">
+                        <a style="color: rgb(46, 46, 46);" href="<?= base_url() . 'welcome/index' ?>">
+                            <div>
+                                <span style="padding-right: 10px;" >Kulit</span>
+                                <img style="width: 50px;" src="https://d2qjkwm11akmwu.cloudfront.net/categories/200609_4-3-2020_14-7-9.png"/>
+                            </div>
+                        </a>
+                    </button>
+                </div>
+                <div class="nav-cards_item">
+                    <button class="nav-cards_btn-obat" style="width: 200px; height: 70px;">
+                        <a style="color: rgb(46, 46, 46);" href="<?= base_url() . 'welcome/index' ?>">
+                            <div>
+                                <span style="padding-right: 10px;" >Vitamin</span>
+                                <img style="width: 50px;" src="https://d2qjkwm11akmwu.cloudfront.net/categories/200609_4-3-2020_14-7-9.png"/>
+                            </div>
+                        </a>
+                    </button>
+                </div>
+                <div class="nav-cards_item">
+                    <button class="nav-cards_btn-obat" style="width: 200px; height: 70px;">
+                        <a style="color: rgb(46, 46, 46);" href="<?= base_url() . 'welcome/index' ?>">
+                            <div>
+                                <span style="padding-right: 10px;" >Mata</span>
+                                <img style="width: 50px;" src="https://d2qjkwm11akmwu.cloudfront.net/categories/200609_4-3-2020_14-7-9.png"/>
+                            </div>
+                        </a>
+                    </button>
+                </div>
+                <div class="nav-cards_item">
+                    <button class="nav-cards_btn-obat" style="width: 200px; height: 70px;">
+                        <a style="color: rgb(46, 46, 46);" href="<?= base_url() . 'welcome/index' ?>">
+                            <div>
+                                <span style="padding-right: 10px;" >Jantung</span>
+                                <img style="width: 50px;" src="https://d2qjkwm11akmwu.cloudfront.net/categories/200609_4-3-2020_14-7-9.png"/>
+                            </div>
+                        </a>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Obat Content -->
