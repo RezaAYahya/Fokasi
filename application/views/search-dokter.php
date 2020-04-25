eb904e7e24eecd4bd0d167da7745088c3a724e35
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-25 16:07:26 +0700

    Create view and css search dokter

--- application/views/search-dokter.php
+++ application/views/search-dokter.php
@@ -6,7 +6,7 @@
                     <div class="header-rata-text">
                         <h2>Daftar Dokter</h2>
                         <div class="btn-navi">
-                            <a href="./index.html">Home</a>
+                            <a href="<?= base_url() . 'welcome/index'?>">Home</a>
                             <span>Daftar Dokter</span>
                         </div>
                     </div>
@@ -15,3 +15,42 @@
         </div>
     </div>
     <!-- End Header Rata -->
+    <center>
+        <table class="table" style="width: 100px;">
+            <thead style="background-color: darkgray; width: 10px;">
+                <tr>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Bidang</th>
+                    <th scope="col">Rating</th>
+                    <th scope="col">Contact</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">LordAduy</th scope="row">
+                    <td>aduy</td>
+                    <td>lol123@gmail.com</td>
+                    <td>Spesialis Gigi</td>
+                    <td>6.0</td>
+                    <td>088123456123</td>
+                </tr>
+                <tr>
+                    <th scope="row">LordAduy</th scope="row">
+                    <td>aduy</td>
+                    <td>lol123@gmail.com</td>
+                    <td>Spesialis Gigi</td>
+                    <td>6.0</td>
+                    <td>088123456123</td>
+                </tr>
+                <tr>
+                    <th scope="row">LordAduy</th scope="row">
+                    <td>aduy</td>
+                    <td>lol123@gmail.com</td>
+                    <td>Spesialis Gigi</td>
+                    <td>6.0</td>
+                    <td>088123456123</td>
+                </tr>
+        </table>
+    </center>
