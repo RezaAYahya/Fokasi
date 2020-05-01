a991ae85bbf985f30b9806073262369883c28a94
rezaayahya <rezaahmadyahya@gmail.com>
2020-05-01 12:42:33 +0700

    Add forum css and edit minor error

--- application/views/template/header-dashboard.php
+++ application/views/template/header-dashboard.php
@@ -12,8 +12,6 @@
         <link rel="stylesheet" type="text/css" href="<?= base_url() . $profile ?>" />
     <?php } if (isset($edit_profile)) { ?>
         <link rel="stylesheet" type="text/css" href="<?= base_url() . $edit_profile ?>" />
-    <?php } if (isset($home)) { ?>
-        <link rel="stylesheet" type="text/css" href="<?= base_url() . $home ?>" />
     <?php }?>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
